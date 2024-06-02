b496b11aeff1cf8cae9356ca338445043fff30bd
Adam McKellar <dev@mckellar.eu>
2024-06-02 18:51:20 +0200

    Added simple script to render website from install.cfg. Added pyproject.toml.

---
+++ README.md
@@ -0,0 +1,7 @@
+<div align="center">
+
+# `web-installation-instruction`
+
+**Tool rendering html page from install.cfg file.**
+
+</div>
---
+++ src/__main__.py
@@ -0,0 +1,47 @@
+# Copyright 2024 Adam McKellar, Kanushka Gupta, Timo Ege
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+# http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+import os
+
+from jinja2 import Environment, Template, FileSystemLoader, select_autoescape
+
+from installation_instruction.installation_instruction import InstallationInstruction
+
+
+def main():
+    install = InstallationInstruction.from_file("install.cfg")
+    schema = install.parse_schema()
+
+    if not os.path.exists("public"):
+        os.makedirs("public")
+    else:
+        if os.path.exists("public/index.html"):
+            os.remove("public/index.html")
+
+    env = Environment(
+        loader=FileSystemLoader(["src/template/", "template"]),
+        autoescape=select_autoescape()
+    )
+
+    index = env.get_template("index.html.jinja")
+
+    result = index.render(schema)
+
+    with open("public/index.html", "x") as f:
+        f.write(result)
+
+
+if __name__ == "__main__":
+    main()
